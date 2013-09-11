c55d87f68555786a9f734871d8ba60887e12007b
Szuturon <ronnyszutu@gmail.com>
2013-09-11 15:01:26 -0400

    Generating the products controller and views

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -8,26 +8,4 @@ describe ProductsController do
       response.should be_success
     end
   end
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
 end
