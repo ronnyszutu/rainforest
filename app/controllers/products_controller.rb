ecda87d00fc646fe3ebb14259303c5d60ce66713
Szuturon <ronnyszutu@gmail.com>
2013-09-11 15:16:34 -0400

    Refactoring the products controller to respond to JSON

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,29 @@
 class ProductsController < ApplicationController
   def index
   	@products = Product.all
+
+  	respond_to do |format|
+  		format.html # index.html.erb
+  		format.json { render json: @products }
+  	end
   end
 
   def show
   	@product = Product.find(params[:id])
+
+  	respond_to do |format|
+  		format.html # index.html.erb
+  		format.json { render json: @product }
+  	end
   end
 
   def new
   	@product = Product.new
+
+  	respond_to do |format|
+  		format.html # index.html.erb
+  		format.json { render json: @product }
+  	end
   end
 
   def edit
@@ -18,27 +33,39 @@ class ProductsController < ApplicationController
   def create
   	@product = Product.new product_params
 
-  	if @product.save
-  		redirect_to products_url
-  	else
-  		render :new
+		respond_to do |format|
+  		if @product.save
+  			format.html { redirect_to @product, notice: 'Product was succesfully created.' }
+  			format.json { render json: @product, status: :created, location: @product }
+  		else
+  			format.html { render action: "new" }
+  			format.json { render json: @product.errors, status: :unprocessable_entity }
+  		end
   	end
   end
 
   def update
   	@product = Product.find(params[:id])
 
-  	if @product.update_attributes product_params
-  		redirect_to product_path(@product)
-  	else
-  		render :edit
-  	end
+  	respond_to do |format|
+	  	if @product.update_attributes product_params
+	  		format.html { redirect_to product_path(@product), notice: 'Product was successfully updated.' }
+	  		format.json { head :no_content }
+	  	else
+	  		format.html { render action: "edit" }
+	  		format.json { render json: @product.errors, status: :unprocessable_entity }
+	  	end
+	  end
   end
 
   def destroy
   	@product = Product.find(params[:id])
   	@product.destroy
-  	redirect_to products_path
+  	
+  	respond_to do |format|
+  		format.html { redirect_to products_path }
+  		format.json { head :no_content }
+  	end
   end
 
   private
